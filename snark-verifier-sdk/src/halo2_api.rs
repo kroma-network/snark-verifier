095b5640e43985503e5f5173cccf0ba119cac99c
Ryan Kim <chokobole33@gmail.com>
2024-07-19 13:33:41 +0900

    test(snark-verifier-sdk): add poseidon test

--- snark-verifier-sdk/src/halo2_api.rs
+++ snark-verifier-sdk/src/halo2_api.rs
@@ -319,3 +319,135 @@ where
 {
     verify_snark::<ConcreteCircuit, VerifierGWC<_>>(verifier_params, snark, vk)
 }
+
+mod test {
+    use std::io;
+
+    use crate::types::{PoseidonTranscript, POSEIDON_SPEC};
+    use ff::Field;
+    use halo2_base::halo2_proofs::{
+        bn254::SnarkVerifierPoseidonWrite,
+        halo2curves::{
+            bn256::{Bn256, Fr, G1Affine},
+            group::cofactor::CofactorCurveAffine,
+            pairing::Engine,
+        },
+        transcript::{Challenge255, ChallengeScalar, EncodedChallenge, TranscriptWrite},
+    };
+    use rand_core::OsRng;
+    use snark_verifier::loader::native::NativeLoader;
+
+    #[derive(Clone, Copy, Debug)]
+    struct Theta;
+    type ChallengeTheta<F> = ChallengeScalar<F, Theta>;
+
+    fn squeeze_challenge<
+        E: Engine,
+        C: EncodedChallenge<E::G1Affine>,
+        T: TranscriptWrite<E::G1Affine, C>,
+    >(
+        transcript: &mut T,
+    ) -> ChallengeTheta<E::G1Affine> {
+        transcript.squeeze_challenge_scalar()
+    }
+
+    fn write_point_to_proof<
+        E: Engine,
+        C: EncodedChallenge<E::G1Affine>,
+        T: TranscriptWrite<E::G1Affine, C>,
+    >(
+        transcript: &mut T,
+        point: E::G1Affine,
+    ) -> io::Result<()> {
+        transcript.write_point(point)
+    }
+
+    fn write_scalar_to_proof<
+        E: Engine,
+        C: EncodedChallenge<E::G1Affine>,
+        T: TranscriptWrite<E::G1Affine, C>,
+    >(
+        transcript: &mut T,
+        scalar: E::Scalar,
+    ) -> io::Result<()> {
+        transcript.write_scalar(scalar)
+    }
+
+    #[test]
+    fn test_poseidon_write_scalar_to_proof() {
+        let fr = Fr::random(OsRng);
+        let proof = {
+            let mut transcript = PoseidonTranscript::<NativeLoader, Vec<u8>>::from_spec(
+                vec![],
+                POSEIDON_SPEC.clone(),
+            );
+            write_scalar_to_proof::<Bn256, _, _>(&mut transcript, fr).unwrap();
+            transcript.finalize()
+        };
+        let proof_tachyon = {
+            let mut transcript =
+                SnarkVerifierPoseidonWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
+            write_scalar_to_proof::<Bn256, _, _>(&mut transcript, fr.clone()).unwrap();
+            transcript.finalize()
+        };
+        assert_eq!(proof, proof_tachyon);
+    }
+
+    #[test]
+    fn test_poseidon_write_point_to_proof() {
+        const LEN: i32 = 100;
+        let mut points = (0..LEN).map(|_| G1Affine::random(OsRng)).collect::<Vec<_>>();
+        points.push(G1Affine::identity());
+
+        let proof = {
+            let mut transcript = PoseidonTranscript::<NativeLoader, Vec<u8>>::from_spec(
+                vec![],
+                POSEIDON_SPEC.clone(),
+            );
+            for point in points.iter() {
+                write_point_to_proof::<Bn256, _, _>(&mut transcript, point.clone()).unwrap();
+            }
+            transcript.finalize()
+        };
+        let proof_tachyon = {
+            let mut transcript =
+                SnarkVerifierPoseidonWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
+            for point in points.iter() {
+                write_point_to_proof::<Bn256, _, _>(&mut transcript, point.clone()).unwrap();
+            }
+            transcript.finalize()
+        };
+        assert_eq!(proof, proof_tachyon);
+    }
+
+    #[test]
+    fn test_poseidon_squeeze_challenge() {
+        const LEN: usize = 100;
+        let points: Vec<G1Affine> = (0..LEN).map(|_| G1Affine::random(OsRng)).collect::<Vec<_>>();
+
+        let theta = {
+            let mut transcript = PoseidonTranscript::<NativeLoader, Vec<u8>>::from_spec(
+                vec![],
+                POSEIDON_SPEC.clone(),
+            );
+            for point in points.iter() {
+                write_point_to_proof::<Bn256, _, _>(&mut transcript, point.clone()).unwrap();
+            }
+            let theta = squeeze_challenge::<Bn256, _, _>(&mut transcript);
+            *theta
+        };
+        let theta_tachyon = {
+            let mut transcript: SnarkVerifierPoseidonWrite<
+                Vec<u8>,
+                G1Affine,
+                Challenge255<G1Affine>,
+            > = SnarkVerifierPoseidonWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
+            for point in points.iter() {
+                write_point_to_proof::<Bn256, _, _>(&mut transcript, point.clone()).unwrap();
+            }
+            let theta = squeeze_challenge::<Bn256, _, _>(&mut transcript);
+            *theta
+        };
+        assert_eq!(theta, theta_tachyon);
+    }
+}
